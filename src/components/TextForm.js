9935ad3df898eed2e067574f996597b49f5f8ece
Git Hub <rohanit0002@gmail.com>
2025-05-27 17:34:42 +0530

    Enable Dark mode in whole page

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -31,10 +31,11 @@ export default function TextForm(props) {
 
   return (
     <>
-     <div>
+     <div className="container" style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>
        <h1>{props.heading}</h1>
        <div className='mb-3'>
-        <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+        <textarea className="form-control" value={text} onChange={handleOnChange} 
+        style={{backgroundColor: props.mode=== 'dark' ? 'gray' : 'white', color : props.mode === 'dark' ? 'white' : '#042743' }} id="myBox" rows="8"></textarea>
        </div>
        <button className='btn btn-primary ' onClick={handleUpClick}>Convert to uppercase</button>
        <button className='btn btn-primary mx-3' onClick={handleDownClick}>Convert to lowercase</button>
@@ -43,12 +44,12 @@ export default function TextForm(props) {
         <button className='btn btn-primary mx-3' onClick={handleExtraSpaces}>Remove Extra Spaces</button>
 
     </div>
-    <div className="container my-3">
+    <div className="container my-3"  style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>
        <h1>your text summary</h1>
        <p>{text.split(" ").length} words and {text.length} characters</p>
        <p>{0.008  * text.split(" ").length} Minutes read</p>
        <h2>Preview</h2>
-       <p>{text}</p>
+       <p>{text.length > 0 ? text:"Enter someting in the textbox above to preview in here"}</p>
     </div>
     </>
     
